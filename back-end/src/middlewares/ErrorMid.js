4bd2e4088e4b4261bf56b772e2eb5aada96b6c59
Carlos Alberto Souza Lima Júnior <lima_bigs@yahoo.com.br>
2022-12-01 17:03:29 -0300

    Backend - Login - Cadastro de usuários ok

--- back-end/src/middlewares/ErrorMid.js
+++ back-end/src/middlewares/ErrorMid.js
@@ -1,6 +1,7 @@
 const erro = (err, _req, res, _next) => {
     const { statusCode, message } = err;
-    res.status(statusCode || 500).json({ message });
+    console.log('no errorMid ...>>>>', statusCode, message);
+    return res.status(statusCode || 500).json({ message });
 };
 
 module.exports = {
--- back-end/src/services/userService.js
+++ back-end/src/services/userService.js
@@ -1,3 +1,4 @@
+const md5 = require('md5');
 const { User } = require('../database/models');
 const { loginValidator } = require('../utils/loginValidator');
 const { ErrorGenerator } = require('../utils/ErrorGenerator');
@@ -9,6 +10,26 @@ const findUserByEmail = async ({ email, password }) => {
     return user;
 };
 
+const createUser = async (user) => {
+    if (!user.name) throw new ErrorGenerator(400, 'Required fields are missing');
+    const findUser = await findUserByEmail(user);
+    console.log('busca usuário >>>>>>>>', findUser);
+    if (findUser) throw new ErrorGenerator(404, 'Cliente já cadastrado');
+    console.log('>>>> linha 18 <<<<<');
+    const { name, email, password } = user;
+    if (name.length >= 12) throw new ErrorGenerator(404, 'Dados de cadastro inválidos');
+    console.log('>>>> linha 21 <<<<<');
+    const passwordEncripted = md5(password);
+    console.log('>>>> linha 24 <<<<< :', name, email, passwordEncripted);
+    const userCreated = await User.create(
+        { name, email, password: passwordEncripted, role: 'customer' },
+);
+    console.log('usuário criado >>>>>>>>', userCreated);
+    
+    return userCreated;
+};
+
 module.exports = {
     findUserByEmail,
+    createUser,
 };
