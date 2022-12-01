8f30f66ab446de99521d92f220cd5f311946281b
Carlos Alberto Souza Lima Júnior <lima_bigs@yahoo.com.br>
2022-12-01 19:01:53 -0300

    Backend - Login - Cadastro de usuários - corrigindo pesquisa por nome ou email

--- back-end/src/services/userService.js
+++ back-end/src/services/userService.js
@@ -1,19 +1,20 @@
 const md5 = require('md5');
+const { Op } = require('sequelize');
 const { User } = require('../database/models');
 const { loginValidator } = require('../utils/loginValidator');
 const { ErrorGenerator } = require('../utils/ErrorGenerator');
 
-const findUserByEmail = async ({ email, password }) => {
+const findUserByEmail = async ({ email, password, name = 'xxxxxx' }) => {
     if (!email || !password) throw new ErrorGenerator(400, 'Required fields are missing');
     loginValidator(email, password);
-    const user = await User.findOne({ where: { email } });
+    const user = await User.findOne({ where: { [Op.or]: [{ email }, { name }] } });
     return user;
 };
 
 const createUser = async (user) => {
     if (!user.name) throw new ErrorGenerator(400, 'Required fields are missing');
     const findUser = await findUserByEmail(user);
-    if (findUser) throw new ErrorGenerator(404, 'Cliente já cadastrado');
+    if (findUser) throw new ErrorGenerator(409, 'Conflict');
     const { name, email, password } = user;
     if (name.length < 12) throw new ErrorGenerator(404, 'Dados de cadastro inválidos');
     const passwordEncripted = md5(password);
