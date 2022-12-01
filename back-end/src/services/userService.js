705fc4ec6a9257b83edae8bab97dcf9405382f04
Carlos Alberto Souza Lima Júnior <lima_bigs@yahoo.com.br>
2022-12-01 17:41:19 -0300

    Backend - Login - Cadastro de usuários - últimas correções

--- back-end/src/services/userService.js
+++ back-end/src/services/userService.js
@@ -13,19 +13,13 @@ const findUserByEmail = async ({ email, password }) => {
 const createUser = async (user) => {
     if (!user.name) throw new ErrorGenerator(400, 'Required fields are missing');
     const findUser = await findUserByEmail(user);
-    console.log('busca usuário >>>>>>>>', findUser);
     if (findUser) throw new ErrorGenerator(404, 'Cliente já cadastrado');
-    console.log('>>>> linha 18 <<<<<');
     const { name, email, password } = user;
-    if (name.length >= 12) throw new ErrorGenerator(404, 'Dados de cadastro inválidos');
-    console.log('>>>> linha 21 <<<<<');
+    if (name.length < 12) throw new ErrorGenerator(404, 'Dados de cadastro inválidos');
     const passwordEncripted = md5(password);
-    console.log('>>>> linha 24 <<<<< :', name, email, passwordEncripted);
     const userCreated = await User.create(
         { name, email, password: passwordEncripted, role: 'customer' },
 );
-    console.log('usuário criado >>>>>>>>', userCreated);
-    
     return userCreated;
 };
 
