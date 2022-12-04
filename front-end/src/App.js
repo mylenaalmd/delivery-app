45a714f00c06cb74e061ee41b4ebbf0bda592c5a
Brainon Queiroz <brainonqueiroz@gmail.com>
2022-12-04 16:16:18 -0300

    feat: Adiciona redirecionamento de / para /login

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, Navigate } from 'react-router-dom';
 import Provider from './context/Provider';
 import Login from './pages/Login';
 import Register from './pages/Register';
@@ -9,7 +9,7 @@ function App() {
   return (
     <Provider>
       <Routes>
-        <Route exact path="/" element={ <Login /> } />
+        <Route exact path="/" element={ <Navigate to="/login" /> } />
         <Route exact path="/login" element={ <Login /> } />
         <Route exact path="/register" element={ <Register /> } />
         <Route exact path="/customer/products" element={ <Register /> } />
